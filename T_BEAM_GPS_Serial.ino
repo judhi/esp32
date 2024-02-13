f9e57345d7b4a2819227bf5dde53ea809613e37c
Judhi Prasetyo <judhi@judhi.net>
2024-02-13 20:28:08 +0400

    Update T_BEAM_GPS_Serial.ino

--- T_BEAM_GPS_Serial.ino
+++ T_BEAM_GPS_Serial.ino
@@ -3,14 +3,12 @@
 
 #include <HardwareSerial.h>
 #include <TinyGPS++.h>
-#include <BluetoothSerial.h>
 
 #define GPS_SERIAL_NUM 1
 #define GPS_RX_PIN 34
 #define GPS_TX_PIN 12
 
 HardwareSerial GPSSerial(GPS_SERIAL_NUM);
-BluetoothSerial SerialBT;
 
 TinyGPSPlus gps; // tinyGPSplus lib by Mikal Hart
 
