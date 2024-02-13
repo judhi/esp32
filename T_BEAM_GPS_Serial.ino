88cb09ba2a4592209a0869ae35880d3e0903d9f4
Judhi Prasetyo <judhi@judhi.net>
2024-02-13 20:26:34 +0400

    Update T_BEAM_GPS_Serial.ino

--- T_BEAM_GPS_Serial.ino
+++ T_BEAM_GPS_Serial.ino
@@ -1,12 +1,18 @@
 // Code to get GPS coordinates on TTGO T-BEAM
-// and display in on Serial Monitor
+// and show them on Serial Monitor
 
 #include <HardwareSerial.h>
+#include <TinyGPS++.h>
+#include <BluetoothSerial.h>
+
 #define GPS_SERIAL_NUM 1
 #define GPS_RX_PIN 34
 #define GPS_TX_PIN 12
 
 HardwareSerial GPSSerial(GPS_SERIAL_NUM);
+BluetoothSerial SerialBT;
+
+TinyGPSPlus gps; // tinyGPSplus lib by Mikal Hart
 
 void setup() {
   Serial.begin(115200);
