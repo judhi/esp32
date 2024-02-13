a4265cc48ce1fc2aa797f6b19a528ed356e086c4
Judhi Prasetyo <judhi@judhi.net>
2024-02-13 20:19:48 +0400

    Create T_BEAM_GPS_Serial.ino

---
+++ T_BEAM_GPS_Serial.ino
@@ -0,0 +1,34 @@
+// Code to get GPS coordinates on TTGO T-BEAM
+// and display in on Serial Monitor
+
+#include <HardwareSerial.h>
+#define GPS_SERIAL_NUM 1
+#define GPS_RX_PIN 34
+#define GPS_TX_PIN 12
+
+HardwareSerial GPSSerial(GPS_SERIAL_NUM);
+
+void setup() {
+  Serial.begin(115200);
+  GPSSerial.begin(9600, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+
+  Serial.println("Hello GPS");
+}
+
+void loop() {
+  if (Serial.available()) {
+    char c = Serial.read();
+    GPSSerial.write(c);
+  }
+
+  while (GPSSerial.available()) {
+    if (gps.encode(GPSSerial.read())) {
+      if (gps.location.isValid()) {
+        Serial.print("Latitude/Longitude: ");
+        Serial.print(gps.location.lat(), 6);
+        Serial.print("/");
+        Serial.println(gps.location.lng(), 6);
+      }
+    }
+  }
+}
