e994b2c95d099074970f6d9024092169ff0c376f
Judhi Prasetyo <judhi@judhi.net>
2024-02-13 20:23:12 +0400

    Create T_BEAM_GPS_Bluetooth.ino

---
+++ T_BEAM_GPS_Bluetooth.ino
@@ -0,0 +1,41 @@
+// Code to get GPS coordinates on TTGO T-BEAM
+// and send them to Bluetooth terminal
+
+#include <HardwareSerial.h>
+#include <TinyGPS++.h>
+#include <BluetoothSerial.h>
+
+#define GPS_SERIAL_NUM 1
+#define GPS_RX_PIN 34
+#define GPS_TX_PIN 12
+
+HardwareSerial GPSSerial(GPS_SERIAL_NUM);
+BluetoothSerial SerialBT;
+
+TinyGPSPlus gps; // tinyGPSplus lib by Mikal Hart
+
+String device_name = "JUDHI T-BEAM"; // change this to your own
+
+void setup() {
+  SerialBT.begin(device_name);
+  GPSSerial.begin(9600, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+  SerialBT.println("Hello GPS");
+}
+
+void loop() {
+  if (SerialBT.available()) {
+    char c = SerialBT.read();
+    GPSSerial.write(c);
+  }
+  
+  while (GPSSerial.available()) {
+    if (gps.encode(GPSSerial.read())) {
+      if (gps.location.isValid()) {
+        SerialBT.print("Latitude/Longitude: ");
+        SerialBT.print(gps.location.lat(), 6);
+        SerialBT.print("/");
+        SerialBT.println(gps.location.lng(), 6);
+      }
+    }
+  }
+}
